8b899bb3c1bb783bb1f3428d02cc9b443d74b6c8
jackson meyer <jackson.meyer7@gmail.com>
2017-11-01 02:12:56 -0700

    initial commit, udemy AR, figuring out collectionView and delegate

--- IkeaUserInterface/ViewController.swift
+++ IkeaUserInterface/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.sceneView.session.run(configuration)
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate = self
+        
+    }
+    
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Item", for: indexPath) as! itemCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.green
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.orange
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
---
+++ IkeaUserInterface/itemCell.swift
@@ -0,0 +1,14 @@
+//
+//  itemCell.swift
+//  IkeaUserInterface
+//
+//  Created by jackson Meyer on 11/1/17.
+//  Copyright © 2017 jackson Meyer. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    @IBOutlet weak var itemLabel: UILabel!
+    
+}
